ed053be302bd388aab752efdfe33220d65a8343d
EricHerduin <ericherduin@icloud.com>
2022-09-24 10:13:01 +0200

    port du server mis dans une constante dans .env

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-
+require("dotenv").config();
 const app = require("./app");
 
 const normalizePort = (val) => {
@@ -13,7 +13,7 @@ const normalizePort = (val) => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.PORT || process.env.serverport);
 app.set("port", port);
 
 const errorHandler = (error) => {
